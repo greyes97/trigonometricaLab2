3ed3989e39a37762b52219e4ef4c0938d2089e72
greyes97 <greyesl1@miumg.edu.gt>
2021-05-01 19:39:41 -0600

    First commit added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/trigonometriCalc/TrigonometriCalcApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.trigonometriCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrigonometriCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrigonometriCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/trigonometriCalc/TrigonometriCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.trigonometriCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrigonometriCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
